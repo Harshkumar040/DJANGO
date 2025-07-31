46efabc1a19cdc6038dc2a8011dbfc667534af3e
Harshkumar040 <harshkumar040304@gmail.com>
2025-07-31 11:24:31 +0530

    updated

--- DjangoProject/APP/urls.py
+++ DjangoProject/APP/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('hello/', views.hello, name='hello'),
+    path('var/', views.var),
 ]
--- DjangoProject/APP/views.py
+++ DjangoProject/APP/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def hello(request):
-    return HttpResponse("Hello, World!")
+    return HttpResponse("<h1>Hello, Soneyo!</h1>")
+
+def var(request):
+    name="Harsh"
+    return HttpResponse("The name is "+name)
